74e575a991c73a70a0c21a062d7fc4c91905ce56
PRATIK <pratiksingh1499@gmail.com>
2023-08-01 23:31:56 +0530

    changing model

--- app.py
+++ app.py
@@ -1,72 +1,45 @@
 import os
 import sys
-print(sys.executable)
-
 from flask import Flask, request, redirect, url_for, send_from_directory, render_template
-from tensorflow.keras.layers import Dense, Embedding
-from tensorflow.keras.models import Sequential, model_from_json
-import tensorflow as tf
 import librosa
 import numpy as np
-import math
-from tcn import TCN, tcn_full_summary
-
 from werkzeug.utils import secure_filename
-import numpy as np
+from tensorflow.keras.models import load_model
 
 ALLOWED_EXTENSIONS = set(['wav'])
 UPLOAD_FOLDER = 'uploads'
 
-def load_model():
-    loaded_json = open('model.json', 'r').read()
-    model = model_from_json(loaded_json, custom_objects={'TCN': TCN})
-    model.load_weights('weights.h5')
-    return model
+def load_model_and_label_encoder():
+    model = load_model('audio_classification.hdf5')
+    labelencoder_classes = np.load('labelencoder_classes.npy', allow_pickle=True)
+    label_encoder = {index: label for index, label in enumerate(labelencoder_classes)}
+    return model, label_encoder
 
-model = load_model()
+model, label_encoder = load_model_and_label_encoder()
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-def predict(file):
-    waveform, sample_rate = librosa.load(file, sr=22050, duration=4.0)
+def predict(file_path, top_n=3):
+    audio, sample_rate = librosa.load(file_path, res_type="kaiser_fast")
+    mfccs_features = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
+    mfccs_scaled_features = np.mean(mfccs_features.T, axis=0)
+    mfccs_scaled_features = mfccs_scaled_features.reshape(1, -1)
 
-    # make sure that the audio is sample_rate * 4 seconds long
-    if len(waveform) != (sample_rate * 4):
-        repeat_num = math.ceil((sample_rate*4) / len(waveform))
-        waveform = np.repeat(waveform, repeat_num)
-        waveform = waveform[:(sample_rate*4)]
+    predicted_probs = model.predict(mfccs_scaled_features)[0]
+    top_n_probs_idx = np.argsort(predicted_probs)[::-1][:top_n]
+    top_n_probs = predicted_probs[top_n_probs_idx]
+    top_n_labels = [label_encoder[top_n_probs_idx[i]] for i in range(top_n)]
 
-    matrix = librosa.stft(waveform, center=False, n_fft=1024, hop_length=256)
-
-    matrix = np.stack([np.real(matrix), np.imag(matrix)], -1)
-    matrix = np.swapaxes(matrix, 0, 1)
-    matrix = np.expand_dims(matrix, axis=0)
-    output = model.predict(matrix)
-    output = np.squeeze(output)
-
-    output = {
-        "air_conditioner": output[0],
-        "car_horn": output[1],
-        "children_playing": output[2],
-        "dog_bark": output[3],
-        "drilling": output[4],
-        "engine_idling": output[5],
-        "gun_shot": output[6],
-        "jackhammer": output[7],
-        "siren": output[8],
-        "street_music": output[9]
-    }
-    return output
+    return top_n_labels, top_n_probs
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def template_test():
-    return render_template('home.html', label='', imagesource='file://null')
-
+    return render_template('home.html', labels='', probs='', imagesource='file://null')
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -77,14 +50,12 @@ def upload_file():
             filename = secure_filename(file.filename)
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(file_path)
-            output = predict(file_path)
-            return render_template("home.html", label=output, audiosource=file_path)
-
+            labels, probs = predict(file_path, top_n=1)  # Change `top_n` to the desired number of top classes
+            return render_template("home.html", labels=labels, probs=probs, audiosource=file_path)
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == "__main__":
-    app.run(debug=False,host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0')
